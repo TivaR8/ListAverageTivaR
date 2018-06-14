588c2add1412ca4e5f8607d6c242f7ba1a415097
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-06-14 19:54:03 -0400

    Finished

---
+++ ListAverageTivaR/ListAverageForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ListAverageTivaR
+{
+    partial class frmListAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnCalculateAverage = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(222, 24);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(100, 20);
+            this.txtMark.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(234, 50);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(13, 27);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(162, 13);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Enter a mark between 0 and 100";
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(59, 93);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(192, 173);
+            this.lstMarks.TabIndex = 3;
+            // 
+            // btnCalculateAverage
+            // 
+            this.btnCalculateAverage.Location = new System.Drawing.Point(81, 272);
+            this.btnCalculateAverage.Name = "btnCalculateAverage";
+            this.btnCalculateAverage.Size = new System.Drawing.Size(132, 23);
+            this.btnCalculateAverage.TabIndex = 4;
+            this.btnCalculateAverage.Text = "Calculate Average";
+            this.btnCalculateAverage.UseVisualStyleBackColor = true;
+            this.btnCalculateAverage.Click += new System.EventHandler(this.btnCalculateAverage_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(81, 306);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(85, 13);
+            this.lblAnswer.TabIndex = 5;
+            this.lblAnswer.Text = "Your Average is:";
+            // 
+            // frmListAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 331);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnCalculateAverage);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtMark);
+            this.Name = "frmListAverage";
+            this.Text = "ListAverage By Tiva Rait";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnCalculateAverage;
+        private System.Windows.Forms.Label lblAnswer;
+    }
+}
+
---
+++ ListAverageTivaR/ListAverageForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 14-06-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #39 - Lists
+ * This program lets you enter marks and then determines your grade average
+*/
+
+namespace ListAverageTivaR
+{
+    public partial class frmListAverage : Form
+    {
+        // List of Integers
+        List<int> myListOfIntegers = new List<int>();
+
+        public frmListAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Declare local variables
+            int userMark = -1;
+
+            // Convert the value in the textbook to an integer 
+            try
+            {
+                userMark = int.Parse(txtMark.Text);
+            }
+            // If the user did not enter a valid number, write the error to the command line
+            catch (Exception parseError)
+            {
+                Console.WriteLine("An error occured: '{0}'", parseError);
+            }
+
+            // Check if the user entered a number between 0 and 100
+            if ((userMark >= 0) && (userMark <= 100))
+            {
+                // Add the mark to the listbox
+                this.lstMarks.Items.Add(userMark);
+
+                // Add the mark to the list
+                myListOfIntegers.Add(userMark);
+            }
+            // Otherwise, display an error
+            else
+            {
+                MessageBox.Show("Please enter a number between 0 and 100.", "Invalid Number");
+            }
+        }
+
+        private void btnCalculateAverage_Click(object sender, EventArgs e)
+        {
+            // Define local variables
+            int numberOfMarks = 0;
+            int allGradesAddedTogether = 0;
+            int currentGrade = 0;
+            int answer= 0;
+
+            numberOfMarks = myListOfIntegers.Count;
+
+            // To add all of the grades together
+            for (int counter = 1; counter<= numberOfMarks; counter++)
+            {
+                currentGrade = myListOfIntegers[counter - 1];
+                allGradesAddedTogether = allGradesAddedTogether + currentGrade;
+
+            }
+
+            // Divide the allGrades added together by the number of things in the list
+            answer = allGradesAddedTogether / numberOfMarks;
+
+            lblAnswer.Text = ("Your Average is: " + answer);
+
+        }
+    }
+}
